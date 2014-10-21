179429e511637477a1b4daef3c1c5bf0060895d8
tristan.beaudoin <tristan.beaudoin@MU-C3-24.mullen.pvt.k12.co.us>
2014-10-21 08:55:57 -0600

    school committ

---
+++ Student Information System/src/addingOrDeletingStudents.java
@@ -0,0 +1,15 @@
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class addingOrDeletingStudents {
+
+	 static ArrayList <Students> studentRoster = new ArrayList<Students>();
+	 
+	 Scanner file = new Scanner (new File("studentlist.txt"));
+	 
+	 
+	 
+	 
+}
